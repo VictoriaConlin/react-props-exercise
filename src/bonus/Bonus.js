27ec2e2c40a55037304107d6f3c33b835139c9a1
Victoria Conlin <kinnevic@gmail.com>
2021-11-12 14:11:19 -0500

    Completed the react props exercise

---
+++ src/bonus/Bonus.js
@@ -0,0 +1,9 @@
+function Bonus(Bonus){
+    return (
+        <div>
+         <h1>Bonus Component</h1>
+        </div>
+    );
+   }
+ 
+   export default Bonus;
---
+++ src/exercise/Exercise.js
@@ -0,0 +1,12 @@
+
+function Exercise({good, bad}){
+  
+   return (
+       <div>
+        <h1>{good}</h1>
+        <h1>{bad}</h1>
+       </div>
+   );
+  }
+
+  export default Exercise;
